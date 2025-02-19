b3f5e52cf32921f73b9469e72c818f4a4736ca5a
François Roget <francois@roget.be>
2025-02-19 10:51:21 +0100

    Initial Commit

---
+++ src/test/java/be/rogetConcept/hiLowGame/SampleTest.java
@@ -0,0 +1,10 @@
+package be.rogetConcept.hiLowGame;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+    @Test
+    public void test() {
+        System.out.println("Hello World!");
+    }
+}
